e04777bb18833f70e2fc60e6c8f168d0d56177b8
DYShin1 <125422250+DYShin1@users.noreply.github.com>
2024-07-25 20:26:04 +0900

    'Feat': 로그인
    로그인 기능 구현

--- src/main/java/com/dys/boardserver/controller/UserController.java
+++ src/main/java/com/dys/boardserver/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.dys.boardserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dys.boardserver.dto.UserDTO;
 import com.dys.boardserver.service.impl.UserServiceImpl;
 
 import lombok.extern.log4j.Log4j2;
@@ -18,5 +23,11 @@ public class UserController {
 	@Autowired
 	public UserController(UserServiceImpl userService) {
 		this.userService = userService;
+	}
+
+	@PostMapping("sign-up")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void signUp(@RequestBody UserDTO userDTO){
+		if (UserDTO.hasNull)
 	}
 }
--- src/main/java/com/dys/boardserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/dys/boardserver/service/impl/UserServiceImpl.java
@@ -1,26 +1,57 @@
 package com.dys.boardserver.service.impl;
 
+import static com.dys.boardserver.utils.SHA256Util.*;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dys.boardserver.dto.UserDTO;
+import com.dys.boardserver.exception.DuplicateIdException;
+import com.dys.boardserver.mapper.UserProfileMapper;
 import com.dys.boardserver.service.UserService;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserProfileMapper userProfileMapper;
+
+	public UserServiceImpl(UserProfileMapper userProfileMapper) {
+		this.userProfileMapper = userProfileMapper;
+	}
 
 	@Override
 	public void register(UserDTO userProfile) {
+		boolean dupleIdResult = isDuplicatedId(userProfile.getUserId());
+		if (dupleIdResult){
+			throw new DuplicateIdException("중복된 아이디입니다.");
+		}
+		userProfile.setCreateTime(new Date());
+		userProfile.setPassword(encryptSHA256(userProfile.getPassword()));
+		int insertCount = userProfileMapper.register(userProfile);
 
+		if(insertCount != 1){
+			log.error("insertMember ERROR! {}", userProfile);
+			throw new RuntimeException(
+				"insertUser ERROR! 회원가입 메서드를 확인해주세요\n" + "Params : " + userProfile
+			);
+		}
 	}
 
 	@Override
 	public UserDTO login(String id, String password) {
-		return null;
+		String cryptoPassword = encryptSHA256(password);
+		UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+		return memberInfo;
 	}
 
 	@Override
 	public boolean isDuplicatedId(String id) {
-		return false;
+		return userProfileMapper.idCheck(id) == 1;
 	}
 
 	@Override
@@ -30,11 +61,28 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void updatePassword(String id, String beforePassword, String afterPassword) {
+		String cryptoPassword = encryptSHA256(beforePassword);
+		UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
 
+		if(memberInfo != null){
+			memberInfo.setPassword(encryptSHA256(afterPassword));
+			int insertCount = userProfileMapper.updatePassword(memberInfo);
+		} else{
+			log.error("updatePassword ERROR! {}", memberInfo);
+			throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+		}
 	}
 
 	@Override
 	public void deleteId(String id, String password) {
+		String cryptoPassword = encryptSHA256(password);
+		UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
 
+		if(memberInfo != null){
+			int deleteCount = userProfileMapper.deleteUserProfile(id);
+		} else {
+			log.error("deleteId ERROR! {}", memberInfo);
+			throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+		}
 	}
 }
